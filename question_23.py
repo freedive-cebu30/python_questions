d6187011f379126f11baf64ea60c26f11b30f8c2
Joji <freedive.cebu30@gmail.com>
2022-04-19 23:17:14 +0800

    Create question_23.py

---
+++ question_23.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+
+arr = [1, "a", 2, "b", "c", 3]
+
+
+# forループ
+# for loop
+res = []
+for v in arr:
+  if type(v) is int:
+    res.append(v)
+
+print(res)
+
+
+# filterとlambda式
+# filter and lambda
+answer1 = list(filter(lambda v: type(v) is int, arr))
+print(answer1)
+
+
+# リスト内包表記
+# list Inclusive
+answer2 = [type(v) is int for value in arr]
+print(answer1)
