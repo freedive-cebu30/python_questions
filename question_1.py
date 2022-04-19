cc1acd46932dd6495acf0962836c2981afb506b5
Joji <freedive.cebu30@gmail.com>
2022-04-19 12:11:15 +0800

    Create question_1.py

---
+++ question_1.py
@@ -0,0 +1,31 @@
+number1 = 2
+number2 = 3
+number3 = 10
+number4 = 10_000
+
+# f-string
+print(f'{number3} + {number2} = {number3 + number2}')
+print(f'{number3} - {number2} = {number3 - number2}')
+print(f'{number3} × {number2} = {number3 * number2}')
+print(f'{number4} ÷ {number3} = {number4 / number3}')
+print(f'{number3} ÷ {number2} = {number3 / number2} 余り {number3 % number2}')
+print(f'{number3} ÷ {number2} = {number3 // number2} 余り {number3 % number2}')
+print(f'{number1}の2乗 = {number1 ** 2}')
+print(f'{number1}の3乗 = {number1 ** 3}')
+
+
+# Templateを使って、書くこともできます
+# You can write a code by Template
+from string import Template
+t = Template('${first} + ${second} = ${third}')
+print(t.substitute(first=number3, second=number2, third = number3 + number2))
+
+
+# formatを使って、書くこともできます
+# You can write a code by format
+print('{} - {} = {}'.format(number3, number2, number3 - number2))
+
+
+# %を使って、書くこともできます
+# You can write a code by %
+print('%s * %s = %s' % (number3, number2, number3 * number2))
