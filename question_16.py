4fa25b37dc3d582afcd39dac725a0d2cffcdfbfd
Joji <freedive.cebu30@gmail.com>
2022-04-19 17:21:30 +0800

    Create question_16.py

---
+++ question_16.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+lang = ["ruby", "php", "python", "java", "javascript"]
+print(list(map(str.capitalize, lang)))
+
+
+res = []
+
+for v in lang:
+  res.append(v.capitalize())
+
+print(res)
