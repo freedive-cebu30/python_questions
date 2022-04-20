e33be703c735272e0d8db9c5db059ff9ca33ebbe
Joji <freedive.cebu30@gmail.com>
2022-04-20 19:34:32 +0800

    Update question_40.py

--- question_40.py
+++ question_40.py
@@ -2,7 +2,7 @@
 
 
 fruits = {"apple": 100, "mango": 200, "banana": 400, "orange": 300}
-max_kv = min(fruits.items(), key=lambda v: v[1])
-dct = dict([max_kv])
+min_kv = min(fruits.items(), key=lambda v: v[1])
+dct = dict([min_kv])
 
 print(dct)
