2860297ef3e1117fb28a974eb38a1bdb6f93aeae
Joji <freedive.cebu30@gmail.com>
2022-04-20 21:22:53 +0800

    Update question_43.py

--- question_43.py
+++ question_43.py
@@ -9,5 +9,12 @@ for fruit in fruits:
     res[fruit] += 1
   else:
     res[fruit] = 1
-
+    
 print(res)
+
+res = {} 
+for fruit in fruits:
+  if fruit in res:
+    res[fruit] += 1
+  else:
+    res.setdefault(fruit, 1)
