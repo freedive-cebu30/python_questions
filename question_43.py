460910aa514fc4f87ce6f16df751af81f2eae8a5
Joji <freedive.cebu30@gmail.com>
2022-04-20 21:01:27 +0800

    Create question_43.py

---
+++ question_43.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+
+fruits = ['apple', 'apple', 'orange', 'mango', 'mango', 'mango', 'mango']
+
+res = {}
+for fruit in fruits:
+  if fruit in res:
+    res[fruit] += 1
+  else:
+    res[fruit] = 1
+
+print(res)
