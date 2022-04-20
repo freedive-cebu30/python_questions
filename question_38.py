8a3312357cad3050d432742481588e3187189f89
Joji <freedive.cebu30@gmail.com>
2022-04-20 18:42:31 +0800

    Update question_38.py

--- question_38.py
+++ question_38.py
@@ -1,14 +1,8 @@
 # coding: utf-8
 
 
-dict_people = [{ 'name': 'Taro_1', 'age': 20, 'height': 180, 'weight': 60, 'country': 'Japan' },
-               { 'name': 'Taro_2', 'age': 30, 'height': 170, 'weight': 65 },
-               { 'name': 'Taro_3', 'age': 40, 'height': 160, 'weight': 70, 'country': 'Taiwan'},
-               { 'name': 'Taro_4', 'age': 20, 'height': 160, 'weight': 75, 'country': 'Japan' },
-               { 'name': 'Taro_5', 'age': 60, 'height': 160, 'weight': 80, 'country': 'Japan' },
-               { 'name': 'Taro_6', 'age': 50, 'height': 150, 'weight': 85, 'country': 'Japan' }
-               ]
+fruits = ['apple', 'mango', 'banana']
+prices = [100, 200, 300]
 
-
-sorted_list = sorted(dict_people, key=lambda person: (person['height'], -person['age']))
-print(sorted_list)
+res = dict(zip(fruits, prices))
+print(res)
