e407ff34940764f251ca934e04e7618024dda1a1
Joji <freedive.cebu30@gmail.com>
2022-04-20 17:22:25 +0800

    Update question_33.py

--- question_33.py
+++ question_33.py
@@ -4,6 +4,8 @@
 dict_1 = { 'name': 'Taro', 'age': 20, 'height': 180, 'weight': 60 }
 dict_2 = { 'name_2': 'Jiro', 'age_2': 30, 'height_2': 170, 'weight_2': 50 }
 
+# どちらの方法でも大丈夫です
+# Either way is fine
 dict_3 = {**dict_1, **dict_2}
 print(dict_3)
 
