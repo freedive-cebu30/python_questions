d3e28de73aa2bd8cf92b460ba1172fbe54c5a3df
Joji <freedive.cebu30@gmail.com>
2022-04-20 17:45:51 +0800

    Create question_35.py

---
+++ question_35.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+
+dict_people = [{ 'name': 'Taro_1', 'age': 20, 'height': 180, 'weight': 60, 'country': 'Japan' },
+               { 'name': 'Taro_2', 'age': 30, 'height': 170, 'weight': 65 },
+               { 'name': 'Taro_3', 'age': 40, 'height': 160, 'weight': 70, 'country': 'Taiwan'},
+               { 'name': 'Taro_4', 'age': 50, 'height': 150, 'weight': 75, 'country': 'Japan' }]
+
+res = []
+for person in dict_people:
+  if 'Japan' in person.values():
+    res.append(person)
+
+print(res)
+
+# リスト内包表記
+# list Inclusive
+res2 = [person for person in dict_people if 'Japan' in person.values()]
+print(res2)
