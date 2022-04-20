e398474644c81944eab8eb3f64e3fc7c5521fc5b
Joji <freedive.cebu30@gmail.com>
2022-04-20 11:58:01 +0800

    Create question_26.py

---
+++ question_26.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+
+arr = ["Taiwan-tokyo", "Taiwan-osaka", "Taiwan-kyoto"]
+
+
+# filterとlambda式
+# filter and lambda
+answer1 = list(map(lambda v: v.replace('Taiwan', 'Japan'), arr))
+print(answer1)
+
+
+# リスト内包表記
+# list Inclusive
+answer2 = [v.replace('Taiwan', 'Japan') for v in arr]
+print(answer2)
+
+
+# forループ
+# for loop
+res = []
+for v in arr:
+  val = v.replace('Taiwan', 'Japan')
+  res.append(val)
+
+print(res)
