606f8a422a8b3a45ac4f8ec4f34fdb0e1969df44
Joji <freedive.cebu30@gmail.com>
2022-04-21 17:58:40 +0800

    Create question_57.py

---
+++ question_57.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+
+people = [{ 'name': 'Taro_1', 'age': 20, 'height': 180, 'weight': 60 },
+          { 'name': 'Taro_2', 'age': 40, 'height': 160, 'weight': 70 },
+          { 'name': 'Taro_3', 'age': 50, 'height': 150, 'weight': 75 }
+         ]
+         
+
+class Person():
+  def __init__(self, name, age, height, weight):
+    self.name = name
+    self.age = age
+    self.height = height
+    self.weight = weight
+
+  def bmi(self):
+    print(self.weight / (self.height / 100) ** 2)
+    
+  def show_status(self):
+    print(f'name {self.name}, age: {self.age}')
+
+
+class SuperMan(Person):
+  def fly(self):
+    print(f'{self.name} flying')
+
+
+for person in people:
+  person = SuperMan(person["name"], person["age"], person["height"], person["weight"])
+  person.bmi()
+  person.show_status()
+  person.fly()
