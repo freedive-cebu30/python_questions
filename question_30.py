8436cc176fde5aa1fab043b6e354fc885b60ff2b
Joji <freedive.cebu30@gmail.com>
2022-05-30 23:40:50 +0800

    Update question_30.py

--- question_30.py
+++ question_30.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-
 v_1 = [1, 2, 3, 4, 5, 8, 9]
 v_2 = [9, 7, 6, 5, 4]
 
@@ -8,8 +7,10 @@ set_1 = set(v_1)
 set_2 = set(v_2)
 
 answer = set_1 ^ set_2
+print(answer)
 print(list(answer))
 
 
 answer2 = set_1.symmetric_difference(set_2)
+print(answer2)
 print(list(answer2))
